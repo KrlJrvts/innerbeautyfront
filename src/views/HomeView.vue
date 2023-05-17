af6478b5787bbbc451e50340c5ecfcc8aeb23787
antipaeorg1 <antipaeorg1@hotmail.com>
2023-05-17 19:26:44 +0300

    ANTI dynamical homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,22 @@
 <template>
     <section id="home">
         <div class="d-flex justify-content-end p-4">
-        <a class="btn" @click="pushTo"><i class="fa-solid fa-arrow-right-to-bracket fa-3x"></i></a>
+            <a class="btn" @click="pushTo"><i class="fa-solid fa-arrow-right-to-bracket fa-3x"></i></a>
         </div>
         <div class="container homepage">
             <div class="row">
                 <div class="col">
-                    <h1>Inside beauty Homeview</h1>
+                    <h1>{{ sloganGenerator () }}</h1>
                 </div>
             </div>
         </div>
 
 
     </section>
-  <section>
+    <section>
 
 
-  </section>
+    </section>
 </template>
 
 <script>
@@ -25,23 +25,38 @@
 import router from "@/router";
 
 export default {
-  name: 'HomeView',
-  components: {
+    name: 'HomeView',
+    components: {},
+    data() {
+        return {
+            sloganA: 'Transform Lives, Save Lives: Organ Selling Made Easy.',
+            sloganB: 'Be a Hero, Sell a Part: Join our Organ Selling Community.',
+            sloganC: 'Bringing Hope to Desperate Hearts: The Leading Organ Selling Marketplace.'
+        }
+    },
+    methods: {
+        pushTo() {
+            router.push('/login')
+        },
+        sloganGenerator() {
+            let gen = Math.random();
+            if (gen < 0.3) {
+                return this.sloganA
+            } else if (gen > 0.3 && gen < 0.6) {
+                return this.sloganB;
+            } else {
+                return this.sloganC
+            }
+        },
 
-  },
-  methods:{
-      pushTo() {
-          router.push('/login')
-      },
-
-  }
+    }
 }
 </script>
 
 
 <style>
 
-.container{
+.container {
     width: 100vw;
     height: 40%;
     position: relative;
@@ -51,14 +66,16 @@ export default {
 h1 {
     color: white;
 }
+
 a {
-    color: red!important;
-    transition: all 500ms ease!important;
-    transform: scale(1)!important;
+    color: red !important;
+    transition: all 500ms ease !important;
+    transform: scale(1) !important;
 }
+
 a:hover {
     /*transition-duration: 1000ms!important;*/
-    transform: scale(1.3) perspective(1px)!important;
+    transform: scale(1.3) perspective(1px) !important;
 }
 
 </style>
