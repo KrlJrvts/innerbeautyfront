1c33487415f3ff4b26aa96bb12aeca6fb8d547e3
antipaeorg1 <antipaeorg1@hotmail.com>
2023-05-22 21:13:41 +0300

    Store view more or less completed!

--- src/views/StoreView.vue
+++ src/views/StoreView.vue
@@ -1,12 +1,13 @@
 <template>
     <section>
         <div class="container store-container">
-            <div class="row store-view-row gx-5">
-                <div v-for="category in categories" :key="category.categoryId" :value="category.categoryId" class="col-3">
+            <div class="row store-view-row gx-5 ">
+                <div v-for="category in categories" :key="category.categoryId" :value="category.categoryId"
+                     class="col-3 ">
                     <div class="card store-card bg-transparent" style="width: 100%; height: 100%;">
-                        <img src="https://www.miffy.com/assets/img/icons/og-image-200x200.png" class="card-img-top" >
+                        <img :src="getCategoryImage(category)" class="store-card-image" draggable="false" >
                         <div class="card-body store-card-body bg-dark">
-                            <h2 class="card-text">{{category.categoryName}}</h2>
+                            <h2 class="card-text">{{ category.categoryName }}</h2>
                         </div>
                     </div>
                 </div>
@@ -34,43 +35,71 @@ export default {
 
     },
     methods: {
+        getCategories: function () {
+            this.$http.get("/store")
+                .then(response => {
+                    this.categories = response.data
+                })
+                .catch(() => router.push({name: 'errorRoute'}))
+        },
+        getCategoryImage(category) {
+            switch (category.categoryId) {
+                case 1:
+                    return require("../assets/categoryphotos/kidney.jpg");
+                case 2:
+                    return require("../assets/categoryphotos/hearth.jpg");
+                case 3:
+                    return require("../assets/categoryphotos/lungs.jpg");
+                case 4:
+                    return require("../assets/categoryphotos/liver.jpg");
+            }
 
-            getCategories: function () {
-                this.$http.get("/store")
-                    .then(response => {
-                        this.categories = response.data
-                    })
-                    .catch(()=> router.push({name: 'errorRoute'}))
         }
-
     },
     beforeMount() {
         this.getCategories()
     }
+
 }
 
 </script>
 
 <style scoped>
-.store-view-row {
-    color: white;
-    margin: 0;
-}
+
+
 .card-body {
     color: white;
 }
+
 .store-container {
     position: relative;
-    top: 200px;
+    top: 20%;
 }
+
 .store-card {
-    border-style: solid!important;
-    border-width: 2px!important;
+    border-style: solid !important;
+    border-width: 2px !important;
     border-color: #660000;
+    transform: scale(1);
 }
+.store-card:hover {
+    cursor: pointer;
+    transform: scale(1.1);
+    transition-duration: 900ms;
+}
+
+
 .store-card-body {
-    border-color: #1d1d1d!important;
-    background: rgba(0, 0, 0, .3)!important;
+    border-color: #1d1d1d !important;
+    background: rgba(0, 0, 0, .7) !important;
+
 }
+
+.store-card-image {
+    height: 250px;
+    width: 100%;
+
+}
+
 
 </style>
