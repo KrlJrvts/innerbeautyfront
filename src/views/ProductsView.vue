96c06f10f0e215253eec3884f4d923d75e2e84a0
opilane <student123@gmail.com>
2023-05-24 09:36:56 +0300

    Productsview in process

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,40 +1,81 @@
 <template>
-<section>
-    <div class="container text-center pt-5">
-        <div class="row">
-            <div class="col-3">
-                <div class="d-flex">
-                    <div class="dropdown me-1">
-                        <button type="button" class="btn btn-outline-light button-homepage account-button dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" data-bs-offset="10,20">
-                            Offset
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
+    <section>
+        <div class="container text-center pt-5">
+            <div class="row">
+                <div class="col-3">
+                    <div class="d-flex">
+                        <div class="dropdown me-1">
+                            <button type="button"
+                                    class="btn btn-outline-light button-homepage account-button dropdown-toggle"
+                                    data-bs-toggle="dropdown" aria-expanded="false" data-bs-offset="10,20">
+                                Offset
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </div>
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-outline-light button-homepage account-button">
+                                Reference
+                            </button>
+                            <button type="button"
+                                    class="btn btn-outline-light button-homepage account-button dropdown-toggle dropdown-toggle-split"
+                                    data-bs-toggle="dropdown" aria-expanded="false" data-bs-reference="parent">
+                                <span class="visually-hidden">Toggle Dropdown</span>
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </div>
                     </div>
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-outline-light button-homepage account-button">Reference</button>
-                        <button type="button" class="btn btn-outline-light button-homepage account-button dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false" data-bs-reference="parent">
-                            <span class="visually-hidden">Toggle Dropdown</span>
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
+                </div>
+                <div class="col-9">
+                    <div class="row">
+                        <div class="col-3 ">
+                            <div class="card" style="width: 18rem;">
+                                <img src="" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up
+                                        the
+                                        bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="card" style="width: 18rem;">
+                                <img src="" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up
+                                        the
+                                        bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="card" style="width: 18rem;">
+                                <img src="" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up
+                                        the
+                                        bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-9">
-                <ul class="list-group">
-                    <li class="list-group-item">An item</li>
-                </ul>
-            </div>
         </div>
-    </div>
-  </section>
+    </section>
 </template>
 
 <script>
