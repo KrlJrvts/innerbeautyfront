1a4c5cc8e07a63228a384c41020ed647af63677f
antipaeorg1 <antipaeorg1@hotmail.com>
2023-05-24 11:31:46 +0300

    product view layout WIP

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -34,43 +34,6 @@
                 </div>
             </div>
         </div>
-
-
-        <!--        <div class="container-fluid content-container-product ">-->
-        <!--            <div class="row">-->
-        <!--                <div class="col-4 mt-5 pl-0 pe-0">-->
-        <!--                    <div class="d-flex">-->
-        <!--                        <select class="form-select w-50 m-3 country-select-products"-->
-        <!--                                aria-label="Default select example">-->
-        <!--                            <option selected>All Countries</option>-->
-        <!--                            <option value="1">One</option>-->
-        <!--                            <option value="2">Two</option>-->
-        <!--                            <option value="3">Three</option>-->
-        <!--                        </select>-->
-        <!--                        <select class="col col-3 form-select w-50 m-3 bloodgroup-select-products"-->
-        <!--                                aria-label="Default select example">-->
-        <!--                            <option selected>All Bloodgroups</option>-->
-        <!--                            <option value="1">One</option>-->
-        <!--                            <option value="2">Two</option>-->
-        <!--                            <option value="3">Three</option>-->
-        <!--                        </select>-->
-        <!--                    </div>-->
-        <!--                </div>-->
-        <!--            </div>-->
-        <!--            <div>-->
-        <!--                <div class="col-2 mt-5 pl-0">-->
-        <!--                    <div class="card" style="width: 18rem;">-->
-        <!--                        <img src="" class="card-img-top" alt="...">-->
-        <!--                        <div class="card-body">-->
-        <!--                            <h5 class="card-title">Card title</h5>-->
-        <!--                            <p class="card-text">Some quick example text to build on the card title and make up the bulk-->
-        <!--                                of the card's content.</p>-->
-        <!--                            <a href="#" class="btn btn-primary">Go somewhere</a>-->
-        <!--                        </div>-->
-        <!--                    </div>-->
-        <!--                </div>-->
-        <!--            </div>-->
-        <!--        </div>-->
     </section>
 </template>
 
