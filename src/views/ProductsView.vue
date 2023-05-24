9f1e8de0b7ac557142f0f67bf833f36bc38659d1
antipaeorg1 <antipaeorg1@hotmail.com>
2023-05-24 15:10:29 +0300

    WIP with productview

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -22,14 +22,15 @@
                         <div class="row product-card-row ">
                             <div v-for="product in products" :value="product.productId"
                                  class="card col-2 me-5 mt-5 product-card" style="width: 17rem;">
-                                <img src="" class="card-img-top" alt="...">
-                                <div class="card-body d-block pt-0">
+                                <img :src="product.imageData" class="card-img-top product-card-image" draggable="false">
+                                <div class="card-body d-block pt-2 product-card-body">
                                     <h5 class="card-title">{{ categoryNameGenerator() }}</h5>
                                     <p class="card-text">Removing date: {{product.productAvailableAt}}</p>
                                     <p class="card-text">Previous owner age: {{product.productAge}}</p>
                                     <p class="card-text">Gender: {{product.genderName}}</p>
-                                    <p class="card-text"></p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    <hr class="product-card-separator">
+                                    <a class="btn button-product-favorite"><i class="fa-regular fa-heart fa-2xl me-5"></i></a>
+                                    <a class="btn button-product-cart"><i class="fa-solid fa-basket-shopping fa-2xl"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -107,10 +108,7 @@ export default {
 <style scoped>
 
 
-input {
-    background-color: transparent !important;
-    color: whitesmoke;
-}
+
 .products-container {
 
 }
@@ -139,9 +137,56 @@ input {
     background-color: transparent;
     color: white;
     border-color: #660000;
-    border-radius: 30px;
+    border-radius: 20px!important;
+    padding: 0px;
 }
 
+.product-card:hover .product-card-image {
+    opacity: 1!important;
+    transition-duration: 600ms;
+}
+
+.product-card-image {
+    border-radius: 20px 20px 0px 0 !important;
+    opacity: 0.5!important;
+}
+
+.product-card-body {
+    background: rgba(20, 0, 0, .2)!important;
+}
+.product-card-separator {
+    border-style: dotted;
+    border-width: 0 0 2px;
+    color: rgba(255, 0, 0, 0.6);
+}
+
+.button-product-favorite {
+    padding: 12px!important;
+    color: #660000;
+}
+
+.button-product-favorite:hover {
+    color: #FF0000;
+    scale: 1.1;
+    transition:all 400ms ease-in-out!important;
+
+}
+
+.button-product-cart {
+    padding: 12px!important;
+    color: #660000;
+}
+
+.button-product-cart:hover {
+    color: #FF0000;
+    scale: 1.1;
+    transition:all 400ms ease-in-out!important;
+}
+
+input {
+    background-color: transparent !important;
+    color: whitesmoke;
+}
 input:focus {
     color: white !important;
     border-color: #660000 !important;
