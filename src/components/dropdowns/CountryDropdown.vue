69209190db170dfa123da603b6641429751493ea
antipaeorg1 <antipaeorg1@hotmail.com>
2023-05-28 22:00:43 +0300

    ANTI productsView description modal done

--- src/components/dropdowns/CountryDropdown.vue
+++ src/components/dropdowns/CountryDropdown.vue
@@ -1,5 +1,5 @@
 <template>
-    <select v-model="selectedCountryId" @change="emitSelectedCountryId" class="form-select" aria-label="Default select example">
+    <select v-model="selectedCountryId" @change="emitSelectedCountryId" class="form-select country-select-products" >
         <option v-if="firstOptionName === 'Country'" disabled selected value="0">Country</option>
         <option v-else selected value="0">{{firstOptionName}}</option>
         <option v-for="country in countries" :key=country.countryId :value=country.countryId>{{country.countryName}}</option>
@@ -49,43 +49,12 @@ export default {
 }
 </script>
 <style scoped>
-section {
-    color: black;
+
+.form-select option:checked {
+    color: yellow!important;
+    background-color: yellow!important;
 }
 
-.add-item-container
-input {
-    background-color: transparent !important;
-    color: whitesmoke;
-}
 
-input:focus {
-    color: white !important;
-    border-color: #660000 !important;
-    box-shadow: 0px 0px 500px 10px #660000 !important;
-}
-
-button:hover {
-    background-color: #660000 !important;
-    color: white !important;
-    transition: all 700ms ease !important;
-    transform: scale(1.1) !important;
-    border-color: #660000 !important;
-    box-shadow: 0px 0px 300px 10px #660000;
-}
-
-alert-div {
-    position: relative;
-    margin-top: 200px;
-}
-
-input[type="file"]::-webkit-file-upload-button {
-    padding: 8px 16px; /* Adjust the padding as needed */
-    font-size: 14px; /* Adjust the font size as needed */
-    background-color: black; /* Customize the background color */
-    color: white; /* Customize the text color */
-    border-radius: 4px; /* Add border radius if desired */
-    font-family: inherit;
-}
 
 </style>
