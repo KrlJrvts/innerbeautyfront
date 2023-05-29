429fec7046c8acc42cde6f03b2b0fb614bf0920b
antipaeorg1 <antipaeorg1@hotmail.com>
2023-05-29 19:58:54 +0300

    ANTI favoriteview WIP!

--- src/components/products-in-cart/ProductDataComponent.vue
+++ src/components/products-in-cart/ProductDataComponent.vue
@@ -1,29 +1,30 @@
 <template>
     <tbody>
-    <tr style="height: 200px;">
-        <div class="container py-0 product-data-component">
+    <tr v-for="favoriteProduct in favoriteProducts" :value="favoriteProduct.productId" class="pb-3" style="height: 200px;">
+        <div class="container py-2 mb-4 product-data-component">
             <div class="row ps-0">
                 <div class="col-3 d-flex align-items-center">
-                    <img src="../../assets/accountpictures/thebox.jpeg" style="height: 200px; width: 200px;border-radius: 20px">
+                    <img src="../../assets/accountpictures/thebox.jpeg"
+                         style="height: 200px; width: 200px;border-radius: 20px">
                 </div>
                 <div class="col">
                     <div class="row d-flex justify-content-center">
-                        <h5>Heart</h5>
-                        <div class="row">
+                        <h3>{{favoriteProduct.productName}}</h3>
+                        <div class="row mt-4">
                             <div class="col d-flex justify-content-start ps-0">
-                                <p>Country: </p>
+                                <p>Country: {{favoriteProduct.countryName}} </p>
                             </div>
                             <div class="col d-flex justify-content-center ">
-                                <p>Product age: </p>
+                                <p>Product age: {{favoriteProduct.productAge}} Years</p>
                             </div>
                         </div>
                         <div class="row">
                             <div class="col d-flex justify-content-start ps-0">
-                                <p>Removal date: 22/01/1995</p>
+                                <p>Removal date: {{favoriteProduct.productAvailableAt}}</p>
                             </div>
 
                             <div class="col d-flex justify-content-center">
-                                <p>Blood group: </p>
+                                <p>Blood group: {{favoriteProduct.bloodgroupType}}</p>
                             </div>
                         </div>
                         <div class="row h-25" style="height: 20px">
@@ -32,12 +33,12 @@
                             </div>
                         </div>
                         <div class="col-12 d-flex justify-content-start description-box" style="height: 100px">
-                            <p>adadsadsads</p>
+                            <p>{{ favoriteProduct.productDescription }}</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-2 p-0 d-flex justify-content-start align-items-center">
-                    Price:<span class="price-span ms-1">2000000$</span>
+                    Price:<span class="price-span ms-1">{{favoriteProduct.productPrice}}$</span>
                 </div>
                 <div class="col-1 button-column">
                     <a class="button-cart-remove"><i class="fa-solid fa-xmark fa-3x mt-5 mb-5"></i></a>
@@ -50,14 +51,16 @@
 </template>
 <script>
 import AddItemImage from "@/components/Image/AddItemImage.vue"
+import router from "@/router";
 
 export default {
     name: 'ProductDataComponent',
     components: {AddItemImage},
     data() {
         return {
-            favoriteProduct: [
+            favoriteProducts: [
                 {
+                    productId: 0,
                     productName: '',
                     productAge: 0,
                     productDescription: '',
@@ -72,8 +75,26 @@ export default {
 
         }
     },
-
+    methods: {
+        getCartProducts: function () {
+            this.$http.get("/products/favorite", {
+                    params: {
+                        buyerId: Number(sessionStorage.getItem('userId')),
+                    }
+                }
+            ).then(response => {
+                this.favoriteProducts = response.data
+            }).catch(error => {
+                router.push({name: 'errorRoute'})
+            })
+        },
+    },
+    beforeMount() {
+        this.getCartProducts()
+    }
 }
+
+
 </script>
 <style scoped>
 
@@ -110,15 +131,18 @@ export default {
     transform: scale(1.2);
     transition: all 400ms ease-in-out;
 }
+
 .button-column {
 
     margin-right: 4px;
 }
+
 .description-box {
     word-wrap: break-word;
     white-space: pre-wrap;
     word-break: break-word;
 }
+
 .product-data-component {
 
     border-style: solid;
