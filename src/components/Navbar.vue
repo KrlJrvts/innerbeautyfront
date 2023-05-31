26e4c3b706337e76cffab081897767937e9240f7
Kaarel Järveots <115338028+KrlJrvts@users.noreply.github.com>
2023-05-31 13:24:12 +0300

    KAAREL - COMPLETED

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="row col-12">
         <div class="col d-flex justify-content-start m-1">
-            <img src="../assets/Logo.jpg" style="height: 70px; width: 75px; opacity: 0.5">
         </div>
-        <div class="col-6 d-flex justify-content-center p-2 ">
+        <div class="col-6 d-flex justify-content-center ps-5 p-2">
             <a class="homepage-brand-name" @click="pushToHome">Inner
                 <i class="fa-regular fa-heart fa-beat-fade fa-md"></i>
                 Beauty</a>
@@ -24,25 +23,3 @@ export default {
     }
 }
 </script>
-<style scoped>
-
-section {
-    color: black;
-}
-
-input {
-    background-color: transparent !important;
-    /*background-color: grey;*/
-    color: whitesmoke;
-}
-
-input:focus {
-    color: white !important;
-    border-color: #660000 !important;
-    box-shadow: 0px 0px 500px 10px #660000 !important;
-
-
-}
-
-
-</style>
